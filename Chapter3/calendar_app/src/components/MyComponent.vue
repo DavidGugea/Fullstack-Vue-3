740a8a212c44d2ee141c2e770b27ba3cebb346b2
David Gugea <davidgugea@yahoo.com>
2022-10-11 20:22:47 +0200

    Finished Chapter 3

---
+++ Chapter3/calendar_app/src/components/MyComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <h2>{{ getGreeting }}</h2>
+    <p>This is the Hello World component.</p>
+</template>
+
+<script>
+export default {
+    name: "MyComponent",
+    data() {
+        return {
+            reversedGreeting: '!dlrow olleH'
+        }
+    },
+    computed: {
+        getGreeting() {
+            return this.reversedGreeting.split("").reversed().join("");
+        }
+    }
+}
+</script>
